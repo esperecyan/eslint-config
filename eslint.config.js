166a5a938848d4e137dba54ade245566b0e40f1e
100の人 <100@pokemori.jp>
2024-04-28 17:20:24 +0900

    [開発環境] .gitignoreで指定されているファイルをESLintの対象から除外するように

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import globals from 'globals';
+import { readGitignoreFiles } from 'eslint-gitignore';
 import esperecyanConfig from './index.js';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
@@ -13,6 +14,7 @@ export default [
 				sourceType: 'script',
 			},
 		},
+		ignores: readGitignoreFiles(),
 		rules: {
 			'@stylistic/quote-props': ['warn', 'consistent-as-needed'],
 		},
